4beaaa3e7f9d29af6460dcccecdebde755e644e6
edimez14 <edimez14@gmail.com>
2025-12-01 20:08:18 -0500

    Arreglar error CORS quitando slash final

--- backend/ecommerce/settings.py
+++ backend/ecommerce/settings.py
@@ -99,7 +99,7 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://ecommerce-dc-plantilla.vercel.app/",
+    "https://ecommerce-dc-plantilla.vercel.app",
 ]
 
 # Agregar corsheaders a INSTALLED_APPS y MIDDLEWARE
